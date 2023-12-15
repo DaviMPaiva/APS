b197725fad742d0650029ebafe147125fe3835d8
gugaccampos <gugaccampos13@gmail.com>
2023-12-15 17:10:43 -0300

    Spring Boot Setup

---
+++ AluCar/src/test/java/cin/ufpe/aps/AluCar/AluCarApplicationTests.java
@@ -0,0 +1,13 @@
+package cin.ufpe.aps.AluCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
