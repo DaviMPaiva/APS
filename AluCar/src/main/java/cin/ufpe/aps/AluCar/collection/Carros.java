d273b9d1b9524cba84b294658462c360d1e7e3d2
DaviMPaiva <davimpaiva@outlook.com>
2023-12-16 13:37:33 -0300

    mais skeletos

---
+++ AluCar/src/main/java/cin/ufpe/aps/AluCar/collection/Carros.java
@@ -0,0 +1,11 @@
+package cin.ufpe.aps.AluCar.collection;
+
+import cin.ufpe.aps.AluCar.models.Car;
+
+public class Carros {
+
+    public Car PesquisaCarrosDisponiveis(){
+        //TODO implementa isso aqui
+        return null;
+    }
+}
