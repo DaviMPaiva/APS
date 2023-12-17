c46d7e64a5442dabb9cca92922374ff9b4c17676
DaviMPaiva <davimpaiva@outlook.com>
2023-12-16 22:00:31 -0300

    estou com sono, vou dormir

--- AluCar/src/main/java/cin/ufpe/aps/AluCar/dados/abstractFactory/usuarios/RepositorioUsuarioSql.java
+++ AluCar/src/main/java/cin/ufpe/aps/AluCar/dados/abstractFactory/usuarios/RepositorioUsuarioSql.java
@@ -1,6 +1,9 @@
 package cin.ufpe.aps.AluCar.dados.abstractFactory.usuarios;
 
+import java.util.ArrayList;
+
 import cin.ufpe.aps.AluCar.dados.abstractFactory.DatabaseDAO;
+import cin.ufpe.aps.AluCar.models.Cartao;
 import cin.ufpe.aps.AluCar.models.Usuario;
 
 public class RepositorioUsuarioSql implements IRepositorioUsuarios{
@@ -13,8 +16,11 @@ public class RepositorioUsuarioSql implements IRepositorioUsuarios{
 
     @Override
     public Usuario getUsuario() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getUsuario'");
+        // Mockup 1
+        Cartao cartao1 = new Cartao("1234-5678-9012-3456", "John Doe", "01/25", "123");
+        Usuario usuario1 = new Usuario("John Doe", "password123", "john.doe@example.com", cartao1);
+
+        return usuario1;
     }
 
     @Override
