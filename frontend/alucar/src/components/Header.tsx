51eccbee4d6f465c83e4cb9af1ee423673140aa6
artursanntos <arturcs2001@gmail.com>
2023-12-18 23:16:04 -0300

    front improvement

--- frontend/alucar/src/components/Header.tsx
+++ frontend/alucar/src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div className="flex flex-col items-center">
             <Link to={`/`}>
-                <div className='flex justify-center font-sans text-[4rem] font-bold mb-5 hover:scale-[1.01] ease-in-out duration-300'>Alucar</div>
+                <div className='flex justify-center font-sans text-[4rem] font-bold mb-5 hover:scale-[1.01] ease-in-out duration-300 text-white'>Alucar</div>
             </Link>
         </div>
     )
--- frontend/alucar/src/pages/Historico.tsx
+++ frontend/alucar/src/pages/Historico.tsx
@@ -1,12 +1,22 @@
 import { useContext, useEffect, useState } from 'react';
 import { AlucarContext } from '../contexts/AlucarContext';
 import Header from '../components/Header';
+import { Reserva } from '../types';
 
 export default function Historico() {
 
     const { historico, getHistorico, } = useContext(AlucarContext);
     const handleHistorico = () => {
         getHistorico();
+    }
+
+    const reservation: Reserva = {
+        valor: 150,
+        carro: "GHI9012",
+        dataInicio: "2021-10-10",
+        dataTermino: "2021-10-11",
+        taxa: 0,
+        usuario: "joao@email.com"
     }
 
     return (
@@ -17,7 +27,7 @@ export default function Historico() {
                     <p className='rounded-full bg-red-300 px-3 hover:bg-red-500 text-gray-900 hover:cursor-pointer ease-in-out duration-300'>Visualizar Histórico de Carros Reservados</p>
             </button>
                 {historico.map((reserva, index) => (
-                    <div key={index} className="flex flex-col gap-3 my-4">
+                    <div key={index} className="flex flex-col gap-3 my-4 rounded bg-gray-400 text-white font-semibold p-4 drop-shadow">
                         <p>Placa: {reserva.carro} </p>
                         <p>Valor: {reserva.valor} </p>
                         <p>Tempo: { reserva.dataInicio.toString() } - { reserva.dataTermino.toString() }</p>
