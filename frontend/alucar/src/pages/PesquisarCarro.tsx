5d0ddf27e14566599c9f0f1410e3909520570f06
artursanntos <arturcs2001@gmail.com>
2024-03-04 12:04:44 -0300

    feat: dataInicio and dataTermino come from search

--- frontend/alucar/src/pages/PesquisarCarro.tsx
+++ frontend/alucar/src/pages/PesquisarCarro.tsx
@@ -46,7 +46,8 @@ export default function PesquisarModelo() {
                 </form>
                 <div className="grid grid-cols-3 gap-4">
                     {pesquisaCarros.map((carro, index) => (
-                        <Link to={`/reservar/${carro.placa}`} key={index} className="hover:scale-[1.02] ease-in-out duration-300">
+                        <Link to={`/reservar/${carro.placa}`} state={{ datas: [dataInicio, dataTermino] }}
+                            key={index} className="hover:scale-[1.02] ease-in-out duration-300">
                             <LinhaPesquisaCarros key={index} Carro={carro} />
                         </Link>
                     ))}
